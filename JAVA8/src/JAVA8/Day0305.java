c9259f26bb6a5b90a9f50a379faf96424881847f
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-16 14:08:34 +0900

    3일차: 관계연산자

---
+++ JAVA8/src/JAVA8/Day0305.java
@@ -0,0 +1,26 @@
+package JAVA8;
+
+public class Day0305 {
+	public static void main(String[] args) {
+		int num1 = 10 , num2 = 20 , num3 = 20;
+		System.out.println("num1 = 10 , num2 = 20 , num3 = 20");
+		System.out.println("num1 < num2 :"+ (num1 < num2));
+		System.out.println("num1 <= num2 :"+ (num1 <= num2));
+		System.out.println("num1 > num2 :"+ (num1 > num2));
+		System.out.println("num1 >= num2 :"+ (num1 >= num2));
+		System.out.println("num1 == num2 :"+ (num1 == num2));
+		System.out.println("num1 != num2 :"+ (num1 != num2));
+		System.out.println(); 
+		System.out.println("num3 = 10 , num2 = 20 , num3 = 20");
+		System.out.println("num3 < num2:"+ (num3< num2));     
+		System.out.println("num3 <= num2 :"+ (num3<= num2));   
+		System.out.println("num3 > num2:"+ (num3> num2));     
+		System.out.println("num3 >= num2 :"+ (num3>= num2));   
+		System.out.println("num3 == num2 :"+ (num3== num2));   
+		System.out.println("num3 != num2 :"+ (num3!= num2));    
+		
+		
+		boolean result = num1 <num2;
+		System.out.println(result);
+	}                          
+}
