50b79adf40404181e805dc8e08f7cc4073e8e412
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-16 15:24:15 +0900

    3일차: 조건 연산자

---
+++ JAVA8/src/JAVA8/Day0309.java
@@ -0,0 +1,27 @@
+package JAVA8;
+
+public class Day0309 {
+
+	public static void main(String[] args) {
+		int daegu = 2_000_000;
+		int global = 1_000_000;
+		
+		System.out.println(daegu >= global ? "대구광역시" : "대구시");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
