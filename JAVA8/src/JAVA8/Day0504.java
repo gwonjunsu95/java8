5a6cc4a6144dc128934776c91eb8a4686b192995
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-19 15:03:12 +0900

    5일차: if중첩 사용

---
+++ JAVA8/src/JAVA8/Day0504.java
@@ -0,0 +1,56 @@
+package JAVA8;
+
+public class Day0504 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		String ok ="";
+		if (num ==1) {
+			ok ="찾았습니다.";
+		}else if (num ==2) {
+			ok ="찾았습니다.";
+		}else if (num ==3) {
+			ok ="찾았습니다.";
+		}else if (num ==4) {
+			ok ="찾았습니다.";
+		}else if (num ==5) {
+			ok ="찾았습니다.";
+		}else if (num ==6) {
+			ok ="찾았습니다.";
+		}else if (num ==7) {
+			ok ="찾았습니다.";
+		}else if (num ==8) {
+			ok ="찾았습니다.";
+		}else if (num ==9) {
+			ok ="찾았습니다.";
+		}else{
+			ok ="찾았습니다.";
+		}
+		System.out.println(ok);
+		
+		
+		if (num > 5) {
+			if(num > 7) {
+				if(num > 9) {
+					
+				} else {
+					if(num > 8) {
+						
+					} else {
+						
+					}
+				}
+			} else {
+				if(num > 6) {
+					
+				} else {
+					
+				}
+			}
+		} else {
+			if(num > 2) {
+				
+			}
+		}	
+	}
+}
