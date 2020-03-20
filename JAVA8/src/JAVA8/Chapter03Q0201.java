9120cc8835e9471010eb8ee45d02b40ff891cc23
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-20 15:47:42 +0900

    6일차: Chapter03Q02

---
+++ JAVA8/src/JAVA8/Chapter03Q0201.java
@@ -0,0 +1,24 @@
+package JAVA8;
+
+import java.util.Scanner;
+
+public class Chapter03Q0201 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year;
+		System.out.print("윤년을 판별할 년도 입력! : ");
+		year = sc.nextInt();
+		
+		if( ((year%4 !=0) && (year%100 !=0)) || 
+				((year%4 ==0) && (year%100 ==0)) &&(year%400 == 0) ) {
+			System.out.println(year + "년은 윤년이다.");
+		} else {
+			System.out.println(year + "년은 윤년이 아니다.");
+		}
+		
+		
+		
+		sc.close();
+	}
+}
