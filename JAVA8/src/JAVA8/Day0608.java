17f103384ab4d5c94a25035ca197d8ac3c0b632f
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-20 14:57:38 +0900

    6일차: 반복문 예제5

---
+++ JAVA8/src/JAVA8/Day0608.java
@@ -0,0 +1,12 @@
+package JAVA8;
+
+public class Day0608 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while(true) {	//조건에 true를 입력하면 무한반복문이 됩니다.
+			System.out.println("hello!");
+			i++;
+		}
+	}
+}
