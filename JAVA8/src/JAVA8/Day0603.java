c056daab8e8f08b08caeacc2c3af83cc43f404b6
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-20 13:57:38 +0900

    6일차: 반복문 예제3

---
+++ JAVA8/src/JAVA8/Day0603.java
@@ -0,0 +1,15 @@
+package JAVA8;
+
+public class Day0603 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println("hello!");
+		}
+	}
+}
