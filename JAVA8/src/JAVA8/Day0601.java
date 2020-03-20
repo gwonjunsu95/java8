7ab9f19e0c41ea0b402d6e7fadcfea4f1703208f
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-20 13:20:52 +0900

    6일차: 반복문 예제1

---
+++ JAVA8/src/JAVA8/Day0601.java
@@ -0,0 +1,16 @@
+package JAVA8;
+
+public class Day0601 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int i = 0;
+		
+		for(i = 1; i <= 10 ; i++) {
+			sum += i;
+		}
+		
+		System.out.println("1~10ÀÇ ÇÕ: " + sum);
+		
+	}
+}
