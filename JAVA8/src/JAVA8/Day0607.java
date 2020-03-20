64753e6e40f5ba870349b3507f013accb95c3d0f
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-20 14:53:35 +0900

    6일차: 반복문 while문 시작

---
+++ JAVA8/src/JAVA8/Day0607.java
@@ -0,0 +1,20 @@
+package JAVA8;
+
+public class Day0607 {
+
+	public static void main(String[] args) {
+		for(int i = 0 ; i < 10; i++) {
+			System.out.println(i);
+		}
+		
+		int index = 0;
+		while(index < 10) {
+			System.out.println(index);
+			index++;
+		}
+		
+		
+		
+		
+	}
+}
