532980c492d532c373db608f620156a3de2350e1
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-23 09:37:17 +0900

    7일차: 문제3

---
+++ JAVA8/src/JAVA8/Day0703.java
@@ -0,0 +1,17 @@
+package JAVA8;
+
+public class Day0703 {
+
+	public static void main(String[] args) {
+		int num = 0;
+		System.out.print("1부터 10까지의 합은 :");
+		
+		for(int i = 0; i <= 10 ;i++) {
+			num += i ;
+			
+		}
+		
+		System.out.println(num);
+		
+	}
+}
