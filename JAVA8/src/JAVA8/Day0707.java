4645edbb9c096a77ea86dac92035c5999dd929e0
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-23 10:22:16 +0900

    7일차: 문제7

--- JAVA8/src/JAVA8/Day0707.java
+++ JAVA8/src/JAVA8/Day0707.java
@@ -3,12 +3,12 @@ package JAVA8;
 public class Day0707 {
 
 	public static void main(String[] args) {
-		int num = 0;
+		int num = 0;	//cnt = count 수를 셀때의 변수기본값
 		System.out.print("1부터 50까지의 수 중 3의 배수의 개수는 : ");
 		
 		for(int i = 1; i <= 50; i++) {
 			if (i % 3 ==0) {
-				num +=1;
+				num +=1;	//cnt++ 와 동일
 			}
 		}
 		System.out.println(num + "개 입니다.");
