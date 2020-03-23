129143dd4715f08ad61179f60f6b0bbc65a50647
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-23 13:27:20 +0900

    7일차: 문제3

--- JAVA8/src/JAVA8/Day0703.java
+++ JAVA8/src/JAVA8/Day0703.java
@@ -3,15 +3,15 @@ package JAVA8;
 public class Day0703 {
 
 	public static void main(String[] args) {
-		int num = 0;
+		int sum = 0;
 		System.out.print("1부터 10까지의 합은 :");
 		
 		for(int i = 0; i <= 10 ;i++) {
-			num += i ;
+			sum += i ;
 			
 		}
 		
-		System.out.println(num + "입니다.");
+		System.out.println(sum + "입니다.");
 		
 	}
 }
