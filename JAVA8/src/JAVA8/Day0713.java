2ea8ae0e62aef95e6332a994de28fc661d48541a
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-23 13:25:39 +0900

    7일차: 문제13

---
+++ JAVA8/src/JAVA8/Day0713.java
@@ -0,0 +1,25 @@
+package JAVA8;
+
+import java.util.Scanner;
+
+public class Day0713 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("몇 줄을 출력하시겠습니까? : ");
+		int line = sc.nextInt();
+		
+		for (int i = 1; i <= line; i++) {
+			for (int j = line; j >= i; j--) {
+				if (j % 2 ==0) {
+					System.out.print("$");
+				} else {
+					System.out.print("￦");
+				}
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
