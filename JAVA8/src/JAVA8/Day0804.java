76e64e635475b0d9b663b63b0f63ab4527fe8951
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-24 14:19:58 +0900

    8일차: 문제4

---
+++ JAVA8/src/JAVA8/Day0804.java
@@ -0,0 +1,28 @@
+package JAVA8;
+
+import java.util.Scanner;
+
+public class Day0804 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("몇 줄을 출력하시겠습니까? : ");
+		int line = sc.nextInt();
+		
+		for(int i = 0; i < line; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+}
