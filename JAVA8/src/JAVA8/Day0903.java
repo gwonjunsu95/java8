dabeb8440bc3248142fdaa3e370e8cbe9df64ba6
gwonjunsu <gwonjunsu.dev@gmail.com>
2020-03-25 15:53:36 +0900

    9일차: for문 활용(배열)

--- JAVA8/src/JAVA8/Day0903.java
+++ JAVA8/src/JAVA8/Day0903.java
@@ -39,7 +39,7 @@ public class Day0903 {
 		int cnt = 1;
 		for(int i = 1; i <= 5; i++) {
 			for(int j = 1; j <= 5; j++) {
-				System.out.print(cnt + "\t");
+				//System.out.print(cnt + "\t");
 				System.out.printf("%02d", cnt);
 				cnt++;
 			}
@@ -47,7 +47,7 @@ public class Day0903 {
 		}
 		*/
 		
-		
+		/*
 		for(int i =0; i < 5; i++) {
 			for(int j = 0; j < 5; j++) {
 				if(i % 2 ==0) {
@@ -58,7 +58,19 @@ public class Day0903 {
 			}
 			System.out.println();
 		}
+		*/
 		
+		
+		for(int i = 0 ; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				if(i == j) {
+				System.out.print("@");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
 		
 		
 	}
