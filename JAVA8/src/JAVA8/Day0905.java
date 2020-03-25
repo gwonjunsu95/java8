b13a367209723200c79af9f020641fc0d056e773
K <K@39.116.46.243>
2020-03-25 17:38:24 +0900

    9일차: 숙제

--- JAVA8/src/JAVA8/Day0905.java
+++ JAVA8/src/JAVA8/Day0905.java
@@ -8,6 +8,16 @@ public class Day0905 {
 		 * ABCDEFGHIJKLMNOPQRSTUVWXYZ
 		 힌트: 형변환이 필요 Day0206참고
 		 */
-		
+		char a = 'a';
+		char a2 = 'A';
+		for(int i = 0; i < 26; i++) {
+			char small = (char) (a + i);
+			System.out.print(small + " ");
+		}
+		System.out.println();
+		for(int i = 0; i < 26; i++) {
+			char big = (char) (a2 + i);
+			System.out.print(big + " ");
+		}
 	}
 }
