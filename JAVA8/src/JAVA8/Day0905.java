ba6263499d5a46ef2fe9d95ccad3a8e55c334514
K <K@39.116.46.243>
2020-03-25 17:41:47 +0900

    9일차: 숙제

--- JAVA8/src/JAVA8/Day0905.java
+++ JAVA8/src/JAVA8/Day0905.java
@@ -19,5 +19,22 @@ public class Day0905 {
 			char big = (char) (a2 + i);
 			System.out.print(big + " ");
 		}
+		System.out.println();
+		
+		for(int i = 0; i < 2; i++) {
+			if(i % 2 == 0) {
+				for(int j = 0; j < 26; j++) {
+					char small = (char) (a + j);
+					System.out.print(small + " ");
+				}
+				System.out.println();
+			} else {
+				for(int j = 0; j < 26; j++) {
+					char big = (char) (a2 + j);
+					System.out.print(big + " ");
+				}
+				System.out.println();
+			}
+		}
 	}
 }
