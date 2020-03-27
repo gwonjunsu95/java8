04749a9e6a41402ed58f367c8227efd7563a66cd
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-03-27 13:15:08 +0900

    11일차: chapter3 연습문제

--- JAVA8/src/JAVA8/Day1109.java
+++ JAVA8/src/JAVA8/Day1109.java
@@ -6,6 +6,7 @@ public class Day1109 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("정수를 두 개 입력하세요 : ");
 		int[] arr = new int[2];
 
 		for (int i = 0; i < arr.length; i++) {
