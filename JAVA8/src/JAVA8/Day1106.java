985101e1f01ab7b9df0c0f87dcb8152ef0778519
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-03-27 11:49:45 +0900

    11일차: 로또 번호 생성기

--- JAVA8/src/JAVA8/Day1106.java
+++ JAVA8/src/JAVA8/Day1106.java
@@ -1,6 +1,7 @@
 package JAVA8;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Day1106 {
 
@@ -8,32 +9,38 @@ public class Day1106 {
 		int[] lotto = new int[6];
 		Random rd = new Random();
 
-		for (int i = 0; i < lotto.length; i++) {
-			lotto[i] = rd.nextInt(45) + 1;
-			for(int j = 0; j < i; j++){
-				if(lotto[i] == lotto[j]) {
-					i--;
-					break;
-				}
-			}
-		}
+		System.out.println("로또 생성기");
+		System.out.print("몇 게임을 생성하시겠습니까? : ");
+		Scanner sc = new Scanner(System.in);
+		int game = sc.nextInt();
 
-		int temp = 0;
-		for( int i = 0 ; i < lotto.length; i++) {
-			for (int j = 0; j < i; j++) {
-				if(lotto[i] < lotto[j]) {
-					temp = lotto[i];
-					lotto[i] = lotto[j];
-					lotto[j] = temp;
+		for (int x = 0; x < game; x++) {
+			for (int i = 0; i < lotto.length; i++) {
+				lotto[i] = rd.nextInt(45) + 1;
+				for (int j = 0; j < i; j++) {
+					if (lotto[i] == lotto[j]) {
+						i--;
+						break;
+					}
 				}
 			}
+
+			int temp = 0;
+			for (int i = 0; i < lotto.length; i++) {
+				for (int j = 0; j < i; j++) {
+					if (lotto[i] < lotto[j]) {
+						temp = lotto[i];
+						lotto[i] = lotto[j];
+						lotto[j] = temp;
+					}
+				}
+			}
+
+			for (int i = 0; i < lotto.length; i++) {
+				System.out.print(lotto[i] + " ");
+			}
+			System.out.println();
 		}
-		
-		
-		for(int i = 0; i < lotto.length; i++) {
-			System.out.print(lotto[i]+" ");
-		}
-		
 
 //		for (int i = 0; i < lotto.length; i++) {
 //			for (int j = 0; j < lotto.length; j++) {
