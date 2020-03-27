43ed7301a4beea7ced56abd5c907e1c97c247709
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-03-27 09:26:32 +0900

    11일차: 배열

---
+++ JAVA8/src/JAVA8/Day1101.java
@@ -0,0 +1,36 @@
+package JAVA8;
+
+public class Day1101 {
+
+	public static void main(String[] args) {
+		int[] arr = new int [100];
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + "\t");
+			if ( (i+1) % 5 ==0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+		
+		for(int i = 0; i <arr.length; i++) {
+			arr[i] = i+1;
+		}
+
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + "\t");
+			if ( (i+1) % 5 ==0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+		
+		
+		int sum = 0;
+		for(int i = 0; i < arr.length; i++) {
+			sum +=arr[i];
+		}
+		
+		System.out.println("1~100ÀÇ ÇÕ : " + sum);
+		
+	}
+}
