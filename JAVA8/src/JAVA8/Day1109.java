87d3b2dde11bdb68a99983ef1a9d25c610be8cc3
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-03-27 13:14:16 +0900

    11일차: chapter3 연습문제

---
+++ JAVA8/src/JAVA8/Day1109.java
@@ -0,0 +1,26 @@
+package JAVA8;
+
+import java.util.Scanner;
+
+public class Day1109 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[2];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		if (arr[0] > arr[1]) {
+			int temp = arr[0];
+			arr[0] = arr[1];
+			arr[1] = temp;
+		}
+
+		System.out.println("작은 값 : " + arr[0] + " 큰 값 : " + arr[1]);
+
+		sc.close();
+
+	}
+}
