79a01c180ac70b04bcc623e0f21f4d1070d41a63
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-03-30 12:54:59 +0900

    12일차: 객체

--- JAVA8/src/Day12/Car.java
+++ JAVA8/src/Day12/Car.java
@@ -9,7 +9,56 @@ public class Car {
 	int speed = 0;
 	
 	/* 생성자 */
-	Car(){	//생성자를 생성하지 않으면 기본 생성자가 자동으로 작성됩니다. 
-		
+	Car() {	//생성자를 생성하지 않으면 기본 생성자가 자동으로 작성됩니다. 
+//		this.company = "기아자동차";
+//		this.color = "검정";
+//		this.maxSpeed = 300;
+		this("기아자동차", "검정", 300);
+	}
+	Car(String company, String col, int maxSpeed) {
+		this.company = company;
+		color = col;
+		this.maxSpeed = maxSpeed;
+	}
+	
+	Car(String color, int maxSpeed){
+		this("기아자동차", color, maxSpeed);
+	}
+	
+	Car(int maxSpeed){
+//		this.company = "기아자동차";
+//		this.color = "검정";
+//		this.maxSpeed = maxSpeed;
+		this("기아자동차", "검정", maxSpeed);
+	}
+//	
+//	Car(String company, int maxSpeed , String color){
+//	
+//	}
+//	
+//	Car(String color, int speed , int maxSpeed){
+//	
+//	}
+//	
+//	Car(String company, String color, int maxSpeed, int speed){
+//	
+//	}
+//	
+//	Car(int speed, String color, String company){
+//	
+//	}
+	
+	/*메소드(메서드)*/
+	void accel() {
+		speed += 10;
+		if(speed > maxSpeed) {
+			speed = maxSpeed;
+		}
+		System.out.println("현재 속도" + speed);
+	}
+	
+	void brake() {
+		speed = 0;
+		System.out.println("현재 속도" + speed);
 	}
 }
--- JAVA8/src/Day12/CarEx.java
+++ JAVA8/src/Day12/CarEx.java
@@ -3,7 +3,7 @@ package Day12;
 public class CarEx {
 
 	public static void main(String[] args) {
-		Car myCar = new Car ();	//Car 객체 생성(instance)
+		Car myCar = new Car ("쌍용자동차", "노랑", 350);	//Car 객체 생성(instance)
 
 		System.out.println("데이터 입력 전");
 		System.out.println(myCar.company);
@@ -23,9 +23,35 @@ public class CarEx {
 		System.out.println(yourCar.company);
 		System.out.println(yourCar.maxSpeed);
 		
-		int num1 = 10;
-		int num2 = 10;
-		num1 = 100; 
+		System.out.println("[생성자 예제]");
+		Car c1 = new Car();
+		System.out.println(c1.company);
+		System.out.println(c1.model);
+		System.out.println(c1.color);
+		System.out.println(c1.maxSpeed);
+		System.out.println(c1.speed);
 		
+		Car c2 = new Car(250);
+		System.out.println(c2.company);
+		System.out.println(c2.model);
+		System.out.println(c2.color);
+		System.out.println(c2.maxSpeed);
+		System.out.println(c2.speed);
+		
+		Car c3 = new Car("파랑", 350);
+		System.out.println(c3.company);
+		System.out.println(c3.model);
+		System.out.println(c3.color);
+		System.out.println(c3.maxSpeed);
+		System.out.println(c3.speed);
+		
+		c3.accel();	//메서드 호출
+		c3.speed =325;
+		c3.accel();
+		c3.accel();
+		c3.accel();
+		c3.accel();
+		c3.accel();
+		c3.brake();
 	}
 }
