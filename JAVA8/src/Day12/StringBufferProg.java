2e6600146b40efafc74b8bee1a899f39de14e9ae
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-03-30 15:37:51 +0900

    12일차: StringBuffer

---
+++ JAVA8/src/Day12/StringBufferProg.java
@@ -0,0 +1,23 @@
+package Day12;
+
+public class StringBufferProg {
+
+	public static void main(String[] args) {
+		StringBuffer str = new StringBuffer("java Programming");
+		StringBuffer str2;
+		
+		str2 = str.insert(5,"JSP ");
+		System.out.println(str);
+		System.out.println(str2);
+		
+		str.append(" Good ");
+		str.append('A');
+		System.out.println(str);
+		
+		str2.reverse();
+		System.out.println(str2);
+		
+		str.setLength(10);
+		System.out.println(str);
+	}
+}
