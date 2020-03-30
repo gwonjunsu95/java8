649521f53184b681e6275aa00131e1421bd12ec5
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-03-30 13:12:57 +0900

    12일차: 메서드

--- JAVA8/src/Day12/Person.java
+++ JAVA8/src/Day12/Person.java
@@ -1,5 +1,23 @@
 package Day12;
 
 public class Person {
-
+	void sound() {
+		System.out.println("말하다");
+	}
+	
+	void run() {
+		System.out.println("달리다");
+	}
+	
+	void eat() {
+		System.out.println("음식을 먹다");
+	}
+	
+	void oneDay() {
+//		Person person = new Person();
+//		person.sound();
+		sound();
+		run();
+		eat();
+	}
 }
