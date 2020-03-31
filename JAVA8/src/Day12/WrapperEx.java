77309b18d036a6d008698dc469fbc41f51ac7153
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-03-31 11:15:29 +0900

    13일차: 박싱과 언박싱

---
+++ JAVA8/src/Day12/WrapperEx.java
@@ -0,0 +1,28 @@
+package Day12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WrapperEx {
+
+	public static void main(String[] args) {
+		int number = 10;
+		/* 박싱 */
+		Integer ibox = new Integer(number); // 매개변수는 기본값 또는 문자열을 입력할 수 있다.
+		Integer ibox2 = Integer.valueOf(number);
+
+		/* 언박싱 */
+		number = ibox.intValue();
+
+		/* 자동 박싱 */
+
+		Integer ibox3 = number;
+
+		/* 자동 언박싱 */
+		int result = ibox3 + 10;
+		System.out.println(result);
+		
+		List<Integer> list = new ArrayList();
+
+	}
+}
