7ead71ee49caf6ba40332200e73beecebf36b9a6
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-03-31 15:15:20 +0900

    13일차: public

--- JAVA8/src/Day12/Member.java
+++ JAVA8/src/Day12/Member.java
@@ -5,7 +5,7 @@ public class Member {
 	String name, id, password;
 	int age;
 	
-	Member( String name, String id) {
+	public Member( String name, String id) {
 		this.name = name;
 		this.id = id;
 	}
