fc2aa5cf0fbf0ff8d746a46ceea1b477f5fd12aa
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-03-31 11:02:30 +0900

    13일차: 문제

---
+++ JAVA8/src/Day12/PrinterExample.java
@@ -0,0 +1,18 @@
+package Day12;
+
+public class PrinterExample {
+
+	
+	static void Println(int a){
+		System.out.println(a);
+	}
+	static void Println(boolean a){
+		System.out.println(a);
+	}
+	static void Println(String a){
+		System.out.println(a);
+	}
+	static void Println(double a){
+		System.out.println(a);
+	}
+}
---
+++ JAVA8/src/Day12/PrinterExampleEx.java
@@ -0,0 +1,12 @@
+package Day12;
+
+public class PrinterExampleEx {
+
+	public static void main(String[] args) {
+		PrinterExample.Println(10);
+		PrinterExample.Println(true);
+		PrinterExample.Println(3.14);
+		PrinterExample.Println("Hello");
+		
+	}
+}
