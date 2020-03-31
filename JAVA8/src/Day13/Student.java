f00d8f328a2a069391332ac27c75f1ed58b39e70
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-03-31 16:00:49 +0900

    13일차: getter, setter

---
+++ JAVA8/src/Day13/Student.java
@@ -0,0 +1,77 @@
+package Day13;
+
+public class Student { // DAO(data access object), java bean
+
+	private String name; // 이름
+	private String gender; // 성별
+	private int age; // 나이
+	private int sNo; // 학번
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getsNo() {
+		return sNo;
+	}
+
+	public void setsNo(int sNo) {
+		if(gender.equals("여성")) {
+			sNo += 2000;
+		} else {
+			sNo += 1000;
+		}
+		this.sNo = sNo;
+	}
+
+	public void showInfo() {
+		System.out.println("성명: " + name);
+		System.out.println("성별: " + gender);
+		System.out.println("나이: " + age);
+		System.out.println("학번: " + sNo);
+	}
+
+	
+	
+	
+	
+	
+	public Student(String name, String gender, int age, int sNo) {
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		this.sNo = sNo;
+	}
+
+	public Student() {
+
+	}
+//	String getName() {
+//		return name;
+//	}
+//	
+//	void setName(String name) {
+//		this.name = name;
+//	}
+
+}
