67534c0218c53161eb22c5ad378c96f782dc5608
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-04-01 14:08:48 +0900

    14일차: super.method

---
+++ JAVA8/src/Day14/AirplaneEx.java
@@ -0,0 +1,16 @@
+package Day14;
+
+public class AirplaneEx {
+
+	public static void main(String[] args) {
+		SupersonicAirplane sa = new SupersonicAirplane();
+		sa.takeOff();
+		sa.fly();
+		sa.flyMode = SupersonicAirplane.SUPERSONIC;
+		sa.fly();
+		sa.flyMode = SupersonicAirplane.NORMAL;
+		sa.fly();
+		sa.land();
+		
+	}
+}
