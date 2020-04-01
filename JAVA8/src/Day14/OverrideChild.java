a6d013e2e980396ac3f4c96b560bf5ca573318ba
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-04-01 13:56:05 +0900

    14일차: Override

---
+++ JAVA8/src/Day14/OverrideChild.java
@@ -0,0 +1,12 @@
+package Day14;
+
+public class OverrideChild extends OverrideParent {
+	void method3 () {
+		System.out.println("자식 메서드3");
+	}
+	
+	@Override
+	void method2() {
+		System.out.println("자식 메서드2");
+	}
+}
---
+++ JAVA8/src/Day14/OverrideParent.java
@@ -0,0 +1,12 @@
+package Day14;
+
+public class OverrideParent {
+	void method1 () {
+		System.out.println("부모 메서드1");
+	}
+	
+	void method2 () {
+		System.out.println("부모 메서드2");
+	}
+
+}
