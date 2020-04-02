6b900a35a514a301cd4f5c80cecb265c02cd1992
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-04-02 11:40:15 +0900

    15일차: substring

---
+++ JAVA8/src/Day15/SubstringEx.java
@@ -0,0 +1,14 @@
+package Day15;
+
+public class SubstringEx {
+
+	public static void main(String[] args) {
+		String ssn = "654321-1234567";
+		
+		String firstNum = ssn.substring(0, 6);
+		System.out.println(firstNum);
+		
+		String secondNum = ssn.substring(7);
+		System.out.println(secondNum);
+	}
+}
