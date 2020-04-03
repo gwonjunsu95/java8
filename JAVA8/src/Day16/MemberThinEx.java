ba69bbbfe90175a971fbcbe07a0d5307afaab026
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-04-03 16:27:40 +0900

    16일차: 객체 복제

--- JAVA8/src/Day16/MemberThinEx.java
+++ JAVA8/src/Day16/MemberThinEx.java
@@ -6,8 +6,11 @@ public class MemberThinEx {
 		//원본 객체 생성
 		MemberThin original = new MemberThin("blue", "홍길동", "12345", 25, true);
 		//복제 객체를 얻은 후에 패스워드 변경
-		MemberThin cloned = original.getMember();
+//		MemberThin cloned = original.getMember();	//깊은 복제
+		MemberThin cloned = original;	//얕은 복제
 		cloned.password = "67890";	//복제 객체에서 패스워드 변경
+		
+		System.out.println(original.hashCode() + " : " + cloned.hashCode());
 		
 		System.out.println("[복제 객체의 필드값]");
 		System.out.println("id: " + cloned.id);
