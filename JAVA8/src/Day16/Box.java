bb8ce1ff8dbf7d01e0091ee439b0bfad6c4824de
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-04-03 13:19:04 +0900

    16일차: 클래스의 제네릭

---
+++ JAVA8/src/Day16/Box.java
@@ -0,0 +1,13 @@
+package Day16;
+
+public class Box {
+
+	private Object object;
+	void set (Object object) {
+		this.object = object;
+	}
+	
+	Object get() {
+		return object;
+	}
+}
---
+++ JAVA8/src/Day16/Box2.java
@@ -0,0 +1,13 @@
+package Day16;
+
+public class Box2<T> {	//제네릭은 타입매개변수
+
+	private T object;
+	void set (T object) {
+		this.object = object;
+	}
+	
+	T get() {
+		return object;
+	}
+}
