46f4f2d1d71433cf858d1f6cf24d103edc7b5aa3
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-04-07 14:50:51 +0900

    18일차: supplier

---
+++ JAVA8/src/Day18/SupplierEx.java
@@ -0,0 +1,16 @@
+package Day18;
+
+import java.util.function.IntSupplier;
+
+public class SupplierEx {
+
+	public static void main(String[] args) {
+		IntSupplier intsupplier = () -> {
+			int num = (int) (Math.random() * 6) + 1;
+			return num;
+		};
+		
+		int num = intsupplier.getAsInt();
+		System.out.println("´«ÀÇ ¼ö: " + num);
+	}
+}
