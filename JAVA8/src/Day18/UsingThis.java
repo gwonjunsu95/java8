c33a6bb0ccdf97c611592eea6abc779ba06b1ab5
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-04-07 14:02:38 +0900

    18일차: 람다식

--- JAVA8/src/Day18/UsingThis.java
+++ JAVA8/src/Day18/UsingThis.java
@@ -2,19 +2,31 @@ package Day18;
 
 public class UsingThis {
 	public int outerField = 10;
-	
-	class Inner{
+
+	class Inner {
 		int innerField = 20;
-		
+
 		void method() {
-			
-			Functional fi = () -> {
+
+			Functional fi = () -> {		//람다식은 메서드를생성
 				System.out.println("outerField: " + outerField);
 				System.out.println("outerField: " + UsingThis.this.outerField + "\n");
 				System.out.println("innerField: " + innerField);
 				System.out.println("innerField: " + this.innerField + "\n");
 			};
 			fi.method();
+
+			Functional fi2 = new Functional() {	
+
+				@Override
+				public void method() {	//익명구현객체는 객체를 생성
+					System.out.println("outerField: " + outerField);
+					System.out.println("outerField: " + UsingThis.this.outerField + "\n");
+					System.out.println("innerField: " + innerField);
+					System.out.println("innerField: " + Inner.this.innerField + "\n");
+				}
+			};
+			fi2.method();
 		}
 	}
 
