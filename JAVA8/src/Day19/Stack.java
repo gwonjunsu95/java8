922955789b30fa6b40112e677e37767e51ed7fff
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-04-08 14:39:45 +0900

    19일차: 실습문제1

---
+++ JAVA8/src/Day19/Stack.java
@@ -0,0 +1,27 @@
+package Day19;
+
+public class Stack<T> {
+
+	int position;
+	Object[] stck;
+	public Stack() {
+		position = 0;
+		stck = new Object[20];
+	}
+	//스택에 요소저장
+	public void push(T item) {
+		if(position == 20)
+			return;
+		stck[position] = item;
+		position++;
+	}
+	
+	//스택에 요소 꺼내옴
+	public T pop() {
+		if (position == 0) 
+			return null;
+		position--;
+		return (T)stck[position];
+			
+	}
+}
