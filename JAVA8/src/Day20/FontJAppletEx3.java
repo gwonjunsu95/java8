268dcf4101604f7cfe9c0ddcd8fc8e9a83a72a89
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-04-09 15:06:26 +0900

    20일차: 애플릿 대체 스윙 폰트

--- JAVA8/src/Day20/FontJAppletEx3.java
+++ JAVA8/src/Day20/FontJAppletEx3.java
@@ -11,7 +11,6 @@ public class FontJAppletEx3 {
 		p.init();
 
 		f.setSize(600, 500);
-//		f.setTitle("애플릿 첫 예제");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.add(p);
 		f.setResizable(false);
