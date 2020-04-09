f965ad397971b664b569a5a663b785ef675a4206
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-04-09 09:22:59 +0900

    20일차: 쓰레드 우선순위

---
+++ JAVA8/src/Day20/CalcThread.java
@@ -0,0 +1,16 @@
+package Day20;
+
+public class CalcThread extends Thread{
+	
+	public CalcThread(String name) {
+		setName(name);
+	}
+	
+	public void run() {
+		for(int i =0; i < 2_000_000_000; i++) {
+			
+		}
+		System.out.println(getName());
+	}
+	
+}
---
+++ JAVA8/src/Day20/PriorityEx.java
@@ -0,0 +1,16 @@
+package Day20;
+
+public class PriorityEx {
+
+	public static void main(String[] args) {
+		for(int i =0; i <= 10; i++) {
+			Thread thread = new CalcThread("thread" + i);
+			if(i != 10) {
+				thread.setPriority(Thread.MIN_PRIORITY);
+			} else {
+				thread.setPriority(Thread.MAX_PRIORITY);
+			}
+			thread.start();
+		}
+	}
+}
