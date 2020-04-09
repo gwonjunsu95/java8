289d5d836ddb981cbb57151b6d4892d272281689
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-04-09 15:59:38 +0900

    20일차: 애플릿 대체 스윙 다각형 그리기

---
+++ JAVA8/src/Day20/PolygonJApplet.java
@@ -0,0 +1,26 @@
+package Day20;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class PolygonJApplet extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void paint(Graphics g) {
+		int[] x1 = {50, 25, 60, 90, 55};
+		int[] y1 = {50, 70, 95, 40, 30};
+		int n1 = x1.length;
+		int[] x2 = {130, 105, 140, 170, 135};
+		int[] y2 = {50, 70, 95, 40, 30};
+		int n2 = x2.length;
+		g.setColor(Color.BLUE);
+		g.drawPolygon(x1, y1, n1);
+		g.fillPolygon(x2, y2, n2);
+	}
+}
