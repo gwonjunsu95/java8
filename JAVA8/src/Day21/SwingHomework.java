c85614e966bc600a94ac6a07605d28d8cc8b5b03
K <K@39.116.46.243>
2020-04-10 19:56:51 +0900

    homework

--- JAVA8/src/Day21/SwingHomework.java
+++ JAVA8/src/Day21/SwingHomework.java
@@ -1,6 +1,38 @@
 package Day21;
 
-public class SwingHomework {
+import java.awt.Graphics;
 
-	//스윙을 이용하여 가위바위보 게임 만들기
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SwingHomework extends JFrame {
+
+	static JButton btn1 = new JButton("가위");
+	static JButton btn2 = new JButton("바위");
+	static JButton btn3 = new JButton("보");
+	
+	int com = (int) (Math.random()*3)+1;
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		JPanel p1 = new JPanel();
+		JPanel p2 = new JPanel();
+		
+		p2.add(btn1);
+		p2.add(btn2);
+		p2.add(btn3);
+		p1.add(p2);
+		
+		f.add(p1);
+		f.setSize(300, 200);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setResizable(false);
+		
+	}
 }
