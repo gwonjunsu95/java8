bdeebdf6f25186077761977f19ca4cc2c5daad28
yn-s13 <yn-s13@DESKTOP-OUD91MM>
2020-04-10 15:15:30 +0900

    21일차: JScrollPane

---
+++ JAVA8/src/Day21/JScrollPaneText.java
@@ -0,0 +1,41 @@
+package Day21;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+
+public class JScrollPaneText extends JFrame {
+	JPanel jp;
+	
+	public JScrollPaneText(String title) {
+		super(title);
+		setLayout(new BorderLayout());
+		jp = new JPanel();
+		jp.setLayout(new GridLayout(10, 5));
+		int cnt = 1;
+		for(int i = 1; i <= 10; i++) {
+			for(int j = 1; j <= 5; j++) {
+				jp.add(new JButton("버튼" + cnt));
+				cnt++;
+			}
+		}
+		//수직, 수평 스크롤바를 설정하기 위한 상수를 얻음
+		int v = ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS;
+		int h = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS;
+		JScrollPane js = new JScrollPane(jp, v, h);
+		add(js, BorderLayout.CENTER);
+		
+		setSize(250, 250);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		JScrollPaneText f = new JScrollPaneText("스크롤 예제");
+	}
+}
